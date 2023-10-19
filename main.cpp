d88cc27eb946e4db1715ff0301f82d80afaf8dd6
myrtopar <myrtopar08@gmail.com>
2023-10-19 18:07:15 +0300

    commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+// #include <Map.h>
+
+using namespace std;
+
+// int compare_ints(Pointer a, Pointer b)
+// {
+//     return *(int *)a - *(int *)b;
+// }
+
+int main()
+{
+    // cout << "hi\n";
+    // Map map = map_create(compare_ints, NULL, NULL);
+    // map_set_hash_function(map, hash_int);
+    // map_set_destroy_key(map, NULL);
+    // map_set_destroy_value(map, NULL);
+}
