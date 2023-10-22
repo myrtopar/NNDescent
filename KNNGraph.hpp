bf323e0b65cbef25c19c08f7f39c65a6b9159d91
Danae Karageorgopoulou <79699201+danaikar@users.noreply.github.com>
2023-10-22 17:04:24 +0300

    Add files via upload

--- KNNGraph.hpp
+++ KNNGraph.hpp
@@ -12,6 +12,8 @@ struct MyTuple {
     int num3;
 };
 
+// typedef double (*DistanceFunction)(const void* a, const void* b);
+
 class DataPoint {
 private:
     int id;
@@ -23,7 +25,6 @@ public:
     int getId() const;
     void* getAddr() const;
 };
-
 
 
 class Vertex {
@@ -38,22 +39,102 @@ public:
     void addNeighbor(Vertex* neighborVertex);
     Map getNeighbors() const;
     Map getReverseNeighbors() const;
-
 };
 
 
-
+template <typename DataType, typename DistanceFunction>
 class KNNGraph {
 private:
     Vertex** vertexArray;
-    int size;
     int K;
+    int size;
+    DistanceFunction distance;
 
 public:
-    KNNGraph(int _K, int _size, MyTuple* myTuples);
+    KNNGraph(int _K, int _size, DataType* myTuples, DistanceFunction _distance);
 
     void createRandomGraph(int K, Vertex **vertexArray);
     void printNeighbors() const;
 
     ~KNNGraph();
 };
+
+
+
+
+
+// due to the template usage, the implementation of the functions below should be available in this file
+
+template <typename DataType, typename DistanceFunction>
+KNNGraph<DataType, DistanceFunction>::KNNGraph(int _K, int _size, DataType* myTuples, DistanceFunction _metricFunction) : K(_K), size(_size), distance(_metricFunction) {
+    cout << "\nConstructing a graph of " << size << " elements" << endl;
+    vertexArray = new Vertex*[size];
+    for (int i = 0; i < size; i++) {
+        vertexArray[i] = new Vertex(new DataPoint(i, &myTuples[i]));
+    }
+    createRandomGraph(K, vertexArray);
+}
+
+
+template <typename DataType, typename DistanceFunction>
+void KNNGraph<DataType, DistanceFunction>::createRandomGraph(int K, Vertex **vertexArray) {
+    cout << "\nInitializing the graph..." << endl;
+    // Connect each vertex with K random neighbors
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < K; j++) {
+            int randomNeighborIndex;
+            do {
+                randomNeighborIndex = rand() % size;
+            } while (randomNeighborIndex == i);
+            vertexArray[i]->addNeighbor(vertexArray[randomNeighborIndex]);
+        }
+    }
+    printf("Inserted all neighbors and reverse neighbors\n");
+}
+
+
+template <typename DataType, typename DistanceFunction>
+void KNNGraph<DataType, DistanceFunction>::printNeighbors() const {
+    cout << "\nPrinting Neighbors:" << endl;
+    for (int i = 0; i < size; i++) {
+        Vertex* vertex = vertexArray[i];
+        cout << "Vertex " << i << " neighbors: ";
+        Map neighbors = vertex->getNeighbors();
+        
+        // Iterate through the neighbors and print their IDs
+        MapNode node = map_first(neighbors);
+        while (node != MAP_EOF) {
+            int neighborId = (int)(intptr_t)map_node_key(neighbors, node);
+            cout << neighborId << " ";
+            node = map_next(neighbors, node);
+        }
+        
+        cout << endl;
+    }
+
+    cout << "\nPrinting Reverse Neighbors:" << endl;
+    for (int i = 0; i < size; i++) {
+        Vertex* vertex = vertexArray[i];
+        cout << "Vertex " << i << " reverse neighbors: ";
+        Map neighbors = vertex->getReverseNeighbors();
+        
+        // Iterate through the neighbors and print their IDs
+        MapNode node = map_first(neighbors);
+        while (node != MAP_EOF) {
+            int neighborId = (int)(intptr_t)map_node_key(neighbors, node);
+            cout << neighborId << " ";
+            node = map_next(neighbors, node);
+        }
+        
+        cout << endl;
+    }
+}
+
+
+template <typename DataType, typename DistanceFunction>
+KNNGraph<DataType, DistanceFunction>::~KNNGraph() {
+    for (int i = 0; i < size; i++) {
+        delete vertexArray[i]; // Delete each Vertex
+    }
+    delete[] vertexArray; // Delete the array of Vertex pointers
+}
--- main.cpp
+++ main.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
 #include <cstdlib> 
+#include <cmath>
 #include "KNNGraph.hpp"
 
 using namespace std;
 
 #define K 2
+
+
+// a simple euclidean distance calculator for 3D tuples
+// user can pass any other distance calctulator function
+double calculateEuclideanDistance(const MyTuple& point1, const MyTuple& point2) {
+    double dx = point2.num1 - point1.num1;
+    double dy = point2.num2 - point1.num2;
+    double dz = point2.num3 - point1.num3;
+
+    return std::sqrt(dx * dx + dy * dy + dz * dz);
+}
 
 
 int main() {
@@ -24,10 +36,9 @@ int main() {
         std::cout << "Tuple " << i << ": (" << myTuples[i].num1 << ", " << myTuples[i].num2 << ", " << myTuples[i].num3 << ")\n";
     }
 
-    KNNGraph myGraph(K, arraySize, myTuples);
+    KNNGraph<MyTuple, double (*)(const MyTuple&, const MyTuple&)> myGraph(K, arraySize, myTuples, calculateEuclideanDistance);
     myGraph.printNeighbors();
     
-
     return 0;
 }
 
